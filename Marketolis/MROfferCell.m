cae20a43ee1dae47dea36d7aa86b8d1cd65290d6
Vitaliy Berg <bergusman@gmail.com>
2013-08-22 00:20:49 +0400

    Add OfferCell

---
+++ Marketolis/MROfferCell.m
@@ -0,0 +1,29 @@
+//
+//  MROfferCell.m
+//  Marketolis
+//
+//  Created by Vitaly Berg on 8/22/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import "MROfferCell.h"
+
+@implementation MROfferCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
