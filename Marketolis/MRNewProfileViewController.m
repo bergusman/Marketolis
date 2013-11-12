96eaf4ae8e34adaa4d2c661466852ffcb3dc7b93
Vitaly Berg <bergusman@gmail.com>
2013-11-12 10:32:29 +0400

    Add push methods for profile editor from profile

--- Marketolis/MRNewProfileViewController.m
+++ Marketolis/MRNewProfileViewController.m
@@ -8,11 +8,60 @@
 
 #import "MRNewProfileViewController.h"
 
-@interface MRNewProfileViewController ()
+#import "MRProfileEditorViewController.h"
+
+@interface MRNewProfileViewController () <
+    UITableViewDataSource,
+    UITableViewDelegate
+>
 
 @end
 
 @implementation MRNewProfileViewController
+
+#pragma mark - Setups
+
+- (void)setupNavigationItem {
+    self.navigationItem.title = NSLocalizedString(@"profile.title", @"");
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit
+                                                                                           target:self
+                                                                                           action:@selector(editBarButtonTouchUpInside:)];
+}
+
+#pragma mark - Actions
+
+- (void)editBarButtonTouchUpInside:(id)sender {
+    MRProfileEditorViewController *profileEditorVC = [[MRProfileEditorViewController alloc] init];
+    profileEditorVC.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:profileEditorVC animated:YES];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return NSLocalizedString(@"profile.section.notifications.title", @"");
+}
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupNavigationItem];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -22,10 +71,6 @@
         self.tabBarItem.title = NSLocalizedString(@"tabbar.profile", @"");
     }
     return self;
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
 }
 
 @end
