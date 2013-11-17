e1d76ecc6ba66a4f47cee6df7825b9f8dc9bb640
Vitaly Berg <bergusman@gmail.com>
2013-11-17 21:09:31 +0400

    Create Post Editor View Controller

---
+++ Marketolis/MRPostEditorViewController.m
@@ -0,0 +1,38 @@
+//
+//  MRPostEditorViewController.m
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import "MRPostEditorViewController.h"
+
+@interface MRPostEditorViewController ()
+
+@end
+
+@implementation MRPostEditorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
