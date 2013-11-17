a46a38c47de308ac616c6d0fc470b894b7965b2e
Vitaly Berg <bergusman@gmail.com>
2013-11-17 19:07:32 +0400

    Create Post View Controller

---
+++ Marketolis/MRPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRPostViewController.h
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRPostViewController : UIViewController
+
+@end
