b47c7558cd9485dce1c5b720cd406b540608a983
Vitaly Berg <bergusman@gmail.com>
2013-11-17 20:59:58 +0400

    Add Post Photo Cell

---
+++ Marketolis/MRPostPhotoCell.h
@@ -0,0 +1,15 @@
+//
+//  MRPostPhotoCell.h
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRPostPhotoCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ Marketolis/MRPostPhotoCell.m
@@ -0,0 +1,13 @@
+//
+//  MRPostPhotoCell.m
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import "MRPostPhotoCell.h"
+
+@implementation MRPostPhotoCell
+
+@end
