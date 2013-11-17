72cbc537d793d80c307747f85841ceadf7ba20c2
Vitaly Berg <bergusman@gmail.com>
2013-11-17 20:15:28 +0400

    Work with Post View Controller

--- Marketolis/MRPostViewController.m
+++ Marketolis/MRPostViewController.m
@@ -8,31 +8,54 @@
 
 #import "MRPostViewController.h"
 
-@interface MRPostViewController ()
+@interface MRPostViewController () <
+    UICollectionViewDataSource,
+    UICollectionViewDelegate
+>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (strong, nonatomic) IBOutlet UIView *photosView;
+@property (weak, nonatomic) IBOutlet UICollectionView *photosCollectionView;
+@property (weak, nonatomic) IBOutlet UIPageControl *photosPageControl;
 
 @end
 
 @implementation MRPostViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+#pragma mark - Setups
+
+- (void)setupNavigationItem {
+    self.navigationItem.title = NSLocalizedString(@"post.title", @"");
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"post.edit.button", @"")
+                                                                              style:UIBarButtonItemStyleBordered
+                                                                             target:self
+                                                                             action:@selector(editBarButtonAction:)];
 }
 
-- (void)viewDidLoad
-{
+#pragma mark - Actions
+
+- (void)editBarButtonAction:(id)sender {
+    
+}
+
+#pragma mark -
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self setupNavigationItem];
+    
+    [self.scrollView addSubview:self.photosView];
 }
 
 @end
