7948153b2efb73ac9a52bf431b46358bc62dca08
Vitaly Berg <bergusman@gmail.com>
2013-11-17 18:54:54 +0400

    Create Profile Notification Cell

---
+++ Marketolis/MRProfileNotificationCell.h
@@ -0,0 +1,13 @@
+//
+//  MRProfileNotificationCell.h
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRProfileNotificationCell : UITableViewCell
+
+@end
---
+++ Marketolis/MRProfileNotificationCell.m
@@ -0,0 +1,29 @@
+//
+//  MRProfileNotificationCell.m
+//  Marketolis
+//
+//  Created by Vitaly Berg on 11/17/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import "MRProfileNotificationCell.h"
+
+@implementation MRProfileNotificationCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
