2ddf2a09b17463a920c9351ff87be407b6c707bd
Vitaly Berg <bergusman@gmail.com>
2013-11-30 14:32:11 +0400

    Work with post, layout content of post

--- Marketolis/MRPostViewController.m
+++ Marketolis/MRPostViewController.m
@@ -14,6 +14,8 @@
 
 #import <SDWebImage/UIImageView+WebCache.h>
 
+#import <MapKit/MapKit.h>
+
 @interface MRPostViewController () <
     UIScrollViewDelegate,
     UICollectionViewDataSource,
@@ -22,9 +24,24 @@
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
+// Photos
 @property (strong, nonatomic) IBOutlet UIView *photosView;
 @property (weak, nonatomic) IBOutlet UICollectionView *photosCollectionView;
 @property (weak, nonatomic) IBOutlet UIPageControl *photosPageControl;
+
+// Details
+@property (strong, nonatomic) IBOutlet UIView *detailsView;
+
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailsLabel;
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (weak, nonatomic) IBOutlet UIButton *callButton;
+@property (weak, nonatomic) IBOutlet UIButton *writeButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *viewCountLabel;
 
 @end
 
@@ -45,6 +62,77 @@
     [self.photosCollectionView registerNib:nib forCellWithReuseIdentifier:@"PhotoCell"];
 }
 
+- (void)setupLocalizedStrings {
+    NSLocalizedString(@"", @"");
+}
+
+#pragma mark - Content
+
+- (void)scrollToPhotoAtIndex:(NSInteger)index {
+    // TODO: Check index range
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:index inSection:0];
+    [self.photosCollectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionLeft animated:YES];
+}
+
+- (void)layoutContent {
+    [self layoutDetails];
+    
+    CGRect r = self.detailsView.frame;
+    r.origin.y = 357;
+    self.detailsView.frame = r;
+    
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds.size.width, r.size.height + 357);
+}
+
+- (void)layoutDetails {
+    CGFloat y;
+    CGRect r;
+    
+    y = 40;
+    
+    CGRect titleRect = [self.titleLabel.text boundingRectWithSize:CGSizeMake(300, CGFLOAT_MAX)
+                                                          options:NSStringDrawingUsesLineFragmentOrigin
+                                                       attributes:@{NSFontAttributeName: self.titleLabel.font}
+                                                          context:nil];
+    
+    r = self.titleLabel.frame;
+    r.origin.y = y;
+    r.size.height = ceil(titleRect.size.height);
+    self.titleLabel.frame = r;
+    
+    y += r.size.height + 10;
+    
+    CGRect detailsRect = [self.detailsLabel.text boundingRectWithSize:CGSizeMake(300, CGFLOAT_MAX)
+                                                              options:NSStringDrawingUsesLineFragmentOrigin
+                                                           attributes:@{NSFontAttributeName: self.detailsLabel.font}
+                                                              context:nil];
+    
+    
+    r = self.detailsLabel.frame;
+    r.origin.y = y;
+    r.size.height = ceil(detailsRect.size.height);
+    self.detailsLabel.frame = r;
+    
+    y += r.size.height + 10;
+    
+    r = self.mapView.frame;
+    r.origin.y = y;
+    self.mapView.frame = r;
+    
+    y += r.size.height + 10;
+    
+    r = self.detailsView.frame;
+    r.size.height = y + 300;
+    self.detailsView.frame = r;
+}
+
+- (void)fillDetails {
+    self.dateLabel.text = @"17 августа 2013";
+    self.titleLabel.text = @"MacBook Air mid 2013, 256 GB SSD, 2400 GHz 2-core CPU, 8 GB RAM";
+    self.detailsLabel.text = @"В хорошем состоянии, яркий дисплей, хороший мягкий ход клавишь, глючноватый тачпад, вмятина с боку от падения на роликах, зарядка в коплекте";
+    self.viewCountLabel.text = [NSString stringWithFormat:@"%@: %d", NSLocalizedString(@"post.views", @""), 324];
+}
+
 #pragma mark - Actions
 
 - (void)editBarButtonAction:(id)sender {
@@ -52,7 +140,26 @@
     [self.navigationController pushViewController:editorVC animated:YES];
 }
 
+- (IBAction)photosPageControlValueChanged:(id)sender {
+    [self scrollToPhotoAtIndex:self.photosPageControl.currentPage];
+}
+
+- (IBAction)callButtonTouchUpInside:(id)sender {
+}
+
+- (IBAction)writeButtonTouchUpInside:(id)sender {
+}
+
 #pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView == self.photosCollectionView) {
+        if (scrollView.dragging) {
+            NSInteger index = (NSInteger)(scrollView.contentOffset.x / scrollView.bounds.size.width + 0.5);
+            self.photosPageControl.currentPage = index;
+        }
+    }
+}
 
 #pragma mark - UICollectionViewDataSource
 
@@ -66,13 +173,29 @@
     return cell;
 }
 
+#pragma mark - UICollectionViewDelegate
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    // TODO: show photo in photo viewer
+}
+
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     [self setupNavigationItem];
     [self setupPhotosCollectionView];
+    
     [self.scrollView addSubview:self.photosView];
+    [self.scrollView addSubview:self.detailsView];
+    
+    self.photosPageControl.numberOfPages = 10;
+    
+    self.scrollView.contentSize = CGSizeMake(320, 800);
+    
+    [self fillDetails];
+    [self layoutContent];
 }
 
 @end
