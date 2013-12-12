9bf7101d71c8afd2aca72a92ab9ac8180fc77769
Vitaly Berg <bergusman@gmail.com>
2013-12-12 22:06:37 +0400

    Add generated files and create common Table View Controller

---
+++ Marketolis/MRTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MRTableViewController.h
+//  Marketolis
+//
+//  Created by Vitaly Berg on 12/12/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRTableViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet NSString *analyticTitle;
+
+@end
---
+++ Marketolis/MRTableViewController.m
@@ -0,0 +1,17 @@
+//
+//  MRTableViewController.m
+//  Marketolis
+//
+//  Created by Vitaly Berg on 12/12/13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import "MRTableViewController.h"
+
+@interface MRTableViewController ()
+
+@end
+
+@implementation MRTableViewController
+
+@end
